5a4f0f12aaff51bececed2ea26958493a76f42f8
shinsangsoo <sangsoo9400@gmail.com>
2022-08-18 16:25:04 +0900

    컨테이너 도입, SQL 오류가 console에 보이도록

--- src/com/KoreaIT/example/JAM/dao/ArticleDao.java
+++ src/com/KoreaIT/example/JAM/dao/ArticleDao.java
@@ -1,20 +1,17 @@
 package com.KoreaIT.example.JAM.dao;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import com.KoreaIT.example.JAM.Article;
+import com.KoreaIT.example.JAM.container.Container;
 import com.KoreaIT.example.JAM.util.DBUtil;
 import com.KoreaIT.example.JAM.util.SecSql;
 
 public class ArticleDao {
 
-	private Connection conn;
-
-	public ArticleDao(Connection conn) {
-		this.conn = conn;
+	public ArticleDao() {
 	}
 
 	public int doWrite(String title, String body) {
@@ -26,7 +23,7 @@ public class ArticleDao {
 		sql.append(", title = ?", title);
 		sql.append(", `body` = ?", body);
 
-		int id = DBUtil.insert(conn, sql);
+		int id = DBUtil.insert(Container.conn, sql);
 
 		return id;
 	}
@@ -37,7 +34,7 @@ public class ArticleDao {
 		sql.append("FROM article");
 		sql.append("WHERE id = ?", id);
 
-		return DBUtil.selectRowBooleanValue(conn, sql);
+		return DBUtil.selectRowBooleanValue(Container.conn, sql);
 	}
 
 	public void doDelete(int id) {
@@ -45,7 +42,7 @@ public class ArticleDao {
 		sql.append("DELETE FROM article");
 		sql.append("WHERE id = ?", id);
 
-		DBUtil.delete(conn, sql);
+		DBUtil.delete(Container.conn, sql);
 	}
 
 	public Article getArticleById(int id) {
@@ -55,7 +52,7 @@ public class ArticleDao {
 		sql.append("FROM article");
 		sql.append("WHERE id = ?", id);
 
-		Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+		Map<String, Object> articleMap = DBUtil.selectRow(Container.conn, sql);
 
 		if (articleMap.isEmpty()) {
 			return null;
@@ -73,7 +70,7 @@ public class ArticleDao {
 		sql.append(", `body` = ?", body);
 		sql.append(" WHERE id = ?", id);
 
-		DBUtil.update(conn, sql);
+		DBUtil.update(Container.conn, sql);
 	}
 
 	public List<Article> getArticles() {
@@ -83,7 +80,7 @@ public class ArticleDao {
 		sql.append("FROM article");
 		sql.append("ORDER BY id DESC");
 
-		List<Map<String, Object>> articlesListMap = DBUtil.selectRows(conn, sql);
+		List<Map<String, Object>> articlesListMap = DBUtil.selectRows(Container.conn, sql);
 
 		List<Article> articles = new ArrayList<>();
 
--- src/com/KoreaIT/example/JAM/service/ArticleService.java
+++ src/com/KoreaIT/example/JAM/service/ArticleService.java
@@ -1,16 +1,16 @@
 package com.KoreaIT.example.JAM.service;
 
-import java.sql.Connection;
 import java.util.List;
 
 import com.KoreaIT.example.JAM.Article;
+import com.KoreaIT.example.JAM.container.Container;
 import com.KoreaIT.example.JAM.dao.ArticleDao;
 
 public class ArticleService {
 	private ArticleDao articleDao;
 
-	public ArticleService(Connection conn) {
-		articleDao = new ArticleDao(conn);
+	public ArticleService() {
+		articleDao = Container.articleDao;
 	}
 
 	public int doWrite(String title, String body) {
@@ -37,7 +37,7 @@ public class ArticleService {
 	}
 
 	public List<Article> getArticles() {
-		
+
 		return articleDao.getArticles();
 	}
 
